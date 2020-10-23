3fbcb9a3a8402d79b5560f3bf4e90b76464c3c10
AnthonySosa <tony96sosa@hotmail.com>
2020-10-23 00:40:51 -0300

    Updates

---
+++ precache-manifest.3b3b8afe3f8b354dc8a999cae753a4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7026ac097d6bcda8fbaca874763291b2",
+    "url": "/SocialMediaDashboard-React/index.html"
+  },
+  {
+    "revision": "be1ce61a1945d13052b4",
+    "url": "/SocialMediaDashboard-React/static/css/main.c0344efa.chunk.css"
+  },
+  {
+    "revision": "bd2b9e19296cd39216c7",
+    "url": "/SocialMediaDashboard-React/static/js/2.8ce9c3ec.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/SocialMediaDashboard-React/static/js/2.8ce9c3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1ce61a1945d13052b4",
+    "url": "/SocialMediaDashboard-React/static/js/main.5b4d92a7.chunk.js"
+  },
+  {
+    "revision": "8d481ddd2a81402df049",
+    "url": "/SocialMediaDashboard-React/static/js/runtime-main.f052bbfe.js"
+  }
+]);
